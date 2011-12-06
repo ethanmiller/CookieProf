a4cfad177b057f016f4c5b7138df9d0d2104fde1
Ethan <emiller@energy-solution.com>
2011-12-06 10:14:35 -0800

    Starting cookie stats, hacky timeout
    Apparently the Twisted Agent has no built-in timeout - so it's just a
    scheduled test every 10 to see if the last call started more than 10
    seconds ago.

--- cookieprof.py
+++ cookieprof.py
@@ -26,6 +26,7 @@ from cookielib import CookieJar
 from twisted.internet import reactor
 from twisted.web.client import Agent, CookieAgent
 
+TIMEOUT = 10 # seconds
 # dict keys as their own documentation
 KY_NS_TOTAL = 'no session: total responses, just a count'
 KY_NS_PERCOOK = 'no session: timestamp for unique cookie value responses'
@@ -39,47 +40,60 @@ class PollWindow(object):
         self.cookiejar = CookieJar()
         self.agent = CookieAgent(Agent(reactor), self.cookiejar)
         self.site = site
-        self.stat = StatTracker()
+        self.stats = StatTracker()
         self.win = win
         self.sched_call()
+        self.last_call = datetime.now()
 
     def sched_call(self):
+        now = datetime.now()
+        self.last_call = now
         self.r = self.agent.request('GET', self.site)
         self.r.addCallback(
             self.cbResponse,
             cjar=self.cookiejar,
-            calldt=datetime.now()
+            calldt=now
         )
         self.r.addErrback(self.cbResponse)
         self.update_view()
 
     def update_view(self):
         self.win.addstr(0, 0, self.site)
-        self.win.addstr(1, 0, str(self.stat))
+        self.win.addstr(1, 0, str(self.stats))
         self.win.refresh()
 
     def cbError(self, response):
         pass
 
+    def timeout(self):
+        now = datetime.now()
+        delt = now - self.last_call
+        if delt.seconds > TIMEOUT:
+            self.r.cancel()
+            # log the hit (or miss rather)
+            self.stats.hit(self.last_call)
+            # and try again
+            self.sched_call()
+
     def cbResponse(self, response, **kwargs):
         if 'cjar' not in kwargs.keys():
             return
         # update stats
-        self.stat.hit(kwargs['cjar'], kwargs['calldt'])
+        self.stats.hit(kwargs['calldt'], kwargs['cjar'])
         # if we're "sessioned" reset cookie jar here
         # self.cookiejar = cjar
         self.sched_call() # continuously call
 
 class StatTracker():
     def __init__(self, interesting_key=None):
-        self.ikey = interesting_key
+        self.cstats = CookieTracker(interesting_key)
         self.responses = 0
         self.gaps = []
         self.avg_gap = 0.0
         self.long_gap_dt = None
         self.longest_gap = 0.0
 
-    def hit(self, cook, reqdt):
+    def hit(self, reqdt, cook=None):
         ''' A request came in, collect relevant stats '''
         self.responses += 1
         # First timing related stats
@@ -92,15 +106,44 @@ class StatTracker():
         self.gaps.append(gap)
         self.avg_gap = sum(self.gaps) / len(self.gaps)
         # Cookie stats
-        # ...
+        self.cstats.hit(cook)
 
     def __str__(self):
         return '\n'.join((
             'Total hits: %s' % self.responses,
             'Average response time: %s' % round(self.avg_gap, 2),
             'Slowest response time: %s' % round(self.longest_gap, 2),
-            'Slowest response start: %s' % self.long_gap_dt
+            'Slowest response start: %s' % self.long_gap_dt,
+            '',
+            str(self.cstats)
         ))
+
+class CookieTracker(dict):
+    def __init__(self, interesting_key):
+        self.ikey = interesting_key
+
+    def hit(self, cook):
+        if cook is None:
+            return
+        for c in cook:
+            stats = self.setdefault(c.name, {})
+            hits = stats.setdefault(c.value, [])
+            hits.append(datetime.now())
+
+    def __str__(self):
+        ret = []
+        for kkey, vkey in self.iteritems():
+            ret.append('Cookie: %s' % kkey)
+            tot_hits = sum([len(x) for x in vkey.values()])
+            for kval, vval in vkey.iteritems():
+                val_hits = len(vval)
+                last_seen = vval[-1]
+                ret.extend([
+                    ' - %s' % kval,
+                    '  - %s hits (%s%%)' % (val_hits, 100*(val_hits/tot_hits)),
+                    '  - last seen at %s' % last_seen
+                ])
+        return '\n'.join(ret)
 
 
 if __name__=='__main__':
@@ -133,10 +176,17 @@ if __name__=='__main__':
         polls.append(PollWindow(u, win))
         col_offs += width
 
+    # Admittedly goofy timeout handling :(
+    def run_timeouts():
+        for p in polls:
+            p.timeout()
+        reactor.callLater(10, run_timeouts)
+    reactor.callLater(10, run_timeouts)
+
     def fin_callback(signum, stackframe):
         log = open('log.txt', 'w')
         for p in polls:
-            log.write('~~ %s ~~\n%s\n\n' % (p.site, p.stat))
+            log.write('~~ %s ~~\n%s\n\n' % (p.site, p.stats))
         reactor.stop()
         curses.endwin()
     signal.signal(signal.SIGINT, fin_callback)
